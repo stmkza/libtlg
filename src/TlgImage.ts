7972bb245cc53553012a3a75f73caeeb4ec2aa23
stmkza <stmkza@gmail.com>
2019-06-20 23:30:04 +0900

    TLGの色深度の型を作った

--- src/TlgImage.ts
+++ src/TlgImage.ts
@@ -2,6 +2,7 @@ import { parse } from "path";
 import './DataViewEx';
 
 export type TlgVersion = 'TLG5.0' | 'TLG6.0';
+export type TlgColorDepth = 1 | 3 | 4;
 
 export class TlgImage {
     protected image: ArrayBuffer;
@@ -20,9 +21,9 @@ export class TlgImage {
     parse() {
         // マジックバイトを確認
         this.magicByte = this.view.seekReadString(11);
-        if(this.magicByte === 'TLG5.0\x00raw\x1a') {
+        if (this.magicByte === 'TLG5.0\x00raw\x1a') {
             this.tlgVersion = 'TLG5.0';
-        } else if(this.magicByte === 'TLG6.0\x00raw\x1a') {
+        } else if (this.magicByte === 'TLG6.0\x00raw\x1a') {
             this.tlgVersion = 'TLG6.0';
         } else {
             throw new Error('Unsupported format version.');
